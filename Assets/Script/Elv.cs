ecc30638fd49fd96443aa20211d10e3a3cf668df
Jun <ljhg1124@gmail.com>
2020-01-06 11:42:13 +0900

    first commit
    start!!

---
+++ Assets/Script/Elv.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elv : MonoBehaviour
+{
+    public int isUp;
+    public int isRotZ;
+    private int upDelay;
+
+    void Awake()
+    {
+        isUp = 0;
+        isRotZ = 0;
+        upDelay = 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        switch (isUp)
+        {
+            case 0:
+
+                break;
+
+            case 1: // 올라가기
+                if (upDelay++ > 50)
+                {
+                    transform.Translate(Vector3.up * Time.deltaTime);
+
+                    if (transform.position.y > 0.16f)
+                    {
+                        isUp = 0;
+                        upDelay = 0;
+                        isRotZ = 1;
+                    }
+                }
+                break;
+
+            case 2:
+                if (upDelay++ > 100)
+                {
+                    transform.Translate(-Vector3.up * Time.deltaTime);
+
+                    if (transform.position.y < -3.25f)
+                    {
+                        isUp = 0;
+                        upDelay = 0;
+                    }
+                }
+                break;
+        }
+
+        switch( isRotZ )
+        {
+            case 0:
+
+                break;
+
+            case 1: // 내리기
+                
+                transform.Rotate(-Vector3.forward * Time.deltaTime);
+                if ( transform.rotation.z < -3.0f)
+                {
+                    isRotZ = 0;
+                    isUp = 2;
+                }
+                break;
+
+            case 2: // 올리기
+                break;
+        }
+    }   
+}
---
+++ Assets/Script/rolling.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rolling : MonoBehaviour
+{
+    public GameObject rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void FireBall()
+    {
+        rb.GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(0, 0, -5000));
+        //rb.GetComponent<Rigidbody>().AddForce(new Vector3(10000, 0, 0));
+        //rb.GetComponent<Rigidbody>().AddForceAtPosition(new Vector3(100, 0, 0));
+    }
+}
